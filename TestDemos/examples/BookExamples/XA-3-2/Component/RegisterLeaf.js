421740fc99093c607c1ef1521709d73309d347e9
Azen <azen.cell@foxmail.com>
2016-09-06 16:34:47 +0800

    update for custom view show and 界面间传值

--- TestDemos/examples/BookExamples/XA-3-2/Component/RegisterLeaf.js
+++ TestDemos/examples/BookExamples/XA-3-2/Component/RegisterLeaf.js
@@ -11,13 +11,15 @@ import {
 let {width, height, scale} = Dimensions.get('window');
 let leftMarin = width * 0.1;
 let componentWidth = width * 0.8;
+let ConfirmDialog = require('./ConfirmDialog');
 
 let RegistLeaf = React.createClass({
 
     getInitialState() {
         return {
             inputedNum: '',
-            inputedPW: ''
+            inputedPW: '',
+            needToConfirm: false
         }
     },
 
@@ -48,6 +50,7 @@ let RegistLeaf = React.createClass({
     },
 
     render() {
+        if (this.state.needToConfirm) return this._renderWithDialog();
         return (
             <View style = {styles.container}>
                 <TextInput
@@ -68,12 +71,63 @@ let RegistLeaf = React.createClass({
         )
     },
 
+    _renderWithDialog() {
+        console.log('renderWithDialog');
+        return(
+            <View style = {styles.container}>
+                <TextInput
+                    style={{width: 300, height: 40, backgroundColor: 'gray', alignSelf: 'center', margin: 20}}
+                    placeholder={'请输入手机号'}
+                    onChangeText={(newText => this._updateNum(newText))}
+                />
+                <Text style = {{margin: 20, width: 300, height: 30, fontSize: 16}}>您输入的手机号:{this.state.inputedNum}</Text>
+                <TextInput
+                    placeholder={'请输入密码'}
+                    password={true}
+                    style={{width: 300, height: 40, backgroundColor: 'gray', alignSelf: 'center', margin: 20}}
+                    onChangeText={this._updatePW}
+                />
+                <Text style = {{width: 200, height: 30, borderRadius: 6, backgroundColor: 'black', color: 'white', fontSize: 28, textAlign: 'center'}}
+                      onPress={this._userPressConfirm}>确  定</Text>
+                <ConfirmDialog userConfirmed={this._userConfirmed}
+                                userCanceled={this._userCanceled}
+                                promptToUser={'使用'+this.state.inputedNum+'号码登录?'}/>
+            </View>
+        )
+    },
+
     _userPressConfirm() {
         // this.props.navigator.push({
         //     phoneNumber: this.state.inputedNum,
         //     userPW: this.state.inputedPW,
         //     name:'waiting'
         // });
+        // this.props.navigator.replace({
+        //     phoneNumber: this.state.inputedNum,
+        //     userPW: this.state.inputedPW,
+        //     name:'waiting'
+        // });
+        this.setState((state) => {
+            return {
+                needToConfirm: true
+            };
+        });
+    },
+
+    _userCanceled() {
+        this.setState((state)=>{
+            return {
+                needToConfirm: false
+            };
+        });
+    },
+
+    _userConfirmed() {
+        this.setState((state)=>{
+            return {
+                needToConfirm: false
+            }
+        });
         this.props.navigator.replace({
             phoneNumber: this.state.inputedNum,
             userPW: this.state.inputedPW,
